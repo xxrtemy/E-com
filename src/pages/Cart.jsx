64b0a4cf4279f86d990a349298355398e6608fac
xxrtemy <rebelgamechannel@gmail.com>
2024-03-14 19:05:24 +0300

    modif: add cart heading

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 import Nav from "../components/Nav/Nav.jsx"
 import CartProduct from '../components/Products/CartProduct.jsx'
+import Heading from '../components/ui/Heading.jsx'
 
 const Cart = () => {
   return (
     <div className='h-screen bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-auto'>
       <Nav/>
+      <Heading
+        title="Cart"
+      />
       <CartProduct/>
     </div>
   )
