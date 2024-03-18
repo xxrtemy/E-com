124564430b2d43b728b33c56081948fa9e4525f4
xxrtemy <rebelgamechannel@gmail.com>
2024-03-18 18:10:33 +0300

    modif: users data with axios

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import Login from '../components/Auth/Login'
 
+import axios from 'axios'
 
 
 const Auth = () => {
+  const [data, setData] = React.useState([])
   
-  return(
+  React.useEffect(()=>{
+    axios
+    .get('https://652c0ae1d0d1df5273ef1252.mockapi.io/users')
+    .then((res) => setData(res.data));
+},[setData])
+  
+return(
     <div className="">
-      <Login/>
+      <Login data={data}/>
     </div>
   )
 }
