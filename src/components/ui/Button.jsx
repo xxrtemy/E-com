45080aa147e031ec8fed63a1ee1d6469261802a1
xxrtemy <rebelgamechannel@gmail.com>
2024-03-18 18:08:37 +0300

    modif: btn disabled styles

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -4,8 +4,7 @@ const Button = ({ text, bgColor, textColor, handler, children, disabled = () => 
   return (
     <button
       onClick={handler}
-      className={`${bgColor} ${textColor} ${children} cursor-pointer hover:scale-105 duration-300 py-2 px-8 rounded-full relative z-10`}
-      disabled={disabled}
+      className={`${bgColor} ${textColor} ${children} ${disabled ? "btn-disabled" : ""} cursor-pointer hover:scale-105 duration-300 py-2 px-8 rounded-full relative z-10`}
     >
       {text}
     </button>
