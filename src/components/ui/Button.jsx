83547f6a460d6bbe8a787c1279d25c23e3950af5
xxrtemy <rebelgamechannel@gmail.com>
2024-03-18 16:41:34 +0300

    modif: add prop disabled to button

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-const Button = ({ text, bgColor, textColor, handler, children = () => {} }) => {
+const Button = ({ text, bgColor, textColor, handler, children, disabled = () => {} }) => {
   return (
     <button
       onClick={handler}
       className={`${bgColor} ${textColor} ${children} cursor-pointer hover:scale-105 duration-300 py-2 px-8 rounded-full relative z-10`}
+      disabled={disabled}
     >
       {text}
     </button>
