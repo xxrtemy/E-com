c3bd6867003361d1c009ad8fe90bb0a1201a108c
xxrtemy <rebelgamechannel@gmail.com>
2024-03-18 16:23:28 +0300

    fix: blank page on /auth

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
 import Login from '../components/Auth/Login'
-import Registration from '../components/Auth/Registration'
+
 
 
 const Auth = () => {
-  <div className="">
-    <Route path='/login' element={<Login/>} />
-    <Route path='/registration' element={<Registration/>}/>
-  </div>
+  
+  return(
+    <div className="">
+      <Login/>
+    </div>
+  )
 }
 
 export default Auth
